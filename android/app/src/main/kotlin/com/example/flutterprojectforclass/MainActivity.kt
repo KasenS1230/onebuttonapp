46f80100e850293ae965e56cbd12df3f2bae72bd
KasenS1230 <kasen.sinclair@cox.net>
2026-09-08 15:39:12 -0500

    updated flutter

---
+++ android/app/src/main/kotlin/com/example/flutterprojectforclass/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutterprojectforclass
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Do Nothing Button',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Do Nothing Button'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Intentionally does nothing.
+          },
+          child: const Text('Press Me'),
+        ),
+      ),
+    );
+  }
+}
